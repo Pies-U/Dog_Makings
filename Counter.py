1a41723722ae151e3b3add87e23aa4dfa9efe18d
Pies-U <ppiieessuu@gmail.com>
2022-12-15 22:12:14 +0100

    Update Counter.py

--- Counter.py
+++ Counter.py
@@ -4,21 +4,38 @@ root.geometry("800x600")
 
 #variable is stored in the root object
 root.counter = 0
+set_value = 0
 
-def clicked(click_value):
-    root.counter += click_value
+def clicked1(set_value): 
+    root.counter += set_value
     L['text'] = 'Button clicked: ' + str(root.counter)
-        
-b1 = Button(root, text="+1 Pompka", command=clicked)
+def clicked2():
+    root.counter += 3
+    L['text'] = 'Button clicked: ' + str(root.counter)
+def clicked3():
+    root.counter += 5
+    L['text'] = 'Button clicked: ' + str(root.counter)
+def zero():
+    root.counter = set_value
+    L['text'] = 'Button clicked: ' + str(root.counter)
+
+
+
+
+
+b1 = Button(root, text="  +1 Pompka  ", command=clicked1(20))
 b1.pack()
 
-b2 = Button(root, text="+3 Pompki ", command=clicked)
+b2 = Button(root, text="  +3 Pompki   ", command=clicked2)
 b2.pack()
 
-b3 = Button(root, text="+5 Pompki ", command=clicked)
+b3 = Button(root, text="  +5 Pompki   ", command=clicked3)
 b3.pack()
 
-L = Label(root, text="No clicks yet.")
+b4 = Button(root, text="Zeruj pompki ", command=zero(20))
+b4.pack()
+
+L = Label(root, text="Nie przypisano pompek")
 L.pack()
 
 root.mainloop()
