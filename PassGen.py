105d6f029332f49e1f80afa661862ca32c480838
Pies-U <91903894+Pies-U@users.noreply.github.com>
2022-12-15 21:03:36 +0100

    Create PassGen.py

---
+++ PassGen.py
@@ -0,0 +1,20 @@
+def genpass(size):
+    # Imports
+    import random
+    import string
+
+    # Defines
+    password = ""
+    chars = list(string.ascii_letters+string.digits+string.punctuation)
+
+    # Work
+    while size > 1:
+        password += random.choice(chars)
+        size -= 1
+        random.shuffle(password)
+
+    # Returns    
+    return password
+
+
+print(genpass(15))
