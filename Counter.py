aefc5f26d672635d8ef3adcd1efc98b10e141994
Pies-U <ppiieessuu@gmail.com>
2023-01-03 23:18:50 +0100

    Counter 2.0
    Switch to functions

--- Counter.py
+++ Counter.py
@@ -1,40 +1,55 @@
 from tkinter import *
 root = Tk()
-root.geometry("800x600")
+root.geometry("500x300")
 
-#variable is stored in the root object
-root.counter = 0
-
-def clicked1(): 
-    root.counter += 1
-    L['text'] = 'Button clicked: ' + str(root.counter)
-def clicked2():
-    root.counter += 3
-    L['text'] = 'Button clicked: ' + str(root.counter)
-def clicked3():
-    root.counter += 5
-    L['text'] = 'Button clicked: ' + str(root.counter)
-def zero():
-    root.counter = 0
-    L['text'] = 'Button clicked: ' + str(root.counter)
+counter = 0
+ldata = IntVar()
+pompki = IntVar()
+powod = StringVar()
 
 
+def Error(message):
+    print(message)
 
+def LabelAdd(value):
+    x = ldata.get()
+    ldata.set(x+value)
 
+def LabelSet(value):
+    ldata.set(value)
 
-b1 = Button(root, text="  +1 Pompka  ", command=clicked1)
-b1.pack()
+def FormSubmit():
+    powodTemp = powod.get()
+    pompkiTemp = pompki.get()
+    LabelAdd(pompkiTemp)
+    print(powodTemp)
+    print(pompkiTemp)
 
-b2 = Button(root, text="  +3 Pompki   ", command=clicked2)
-b2.pack()
+def Debug():
+    print()
 
-b3 = Button(root, text="  +5 Pompki   ", command=clicked3)
-b3.pack()
+#FrontEnd
 
-b4 = Button(root, text="Zeruj pompki ", command=zero)
-b4.pack()
+#Buttons
+Debug()
 
-L = Label(root, text="Nie przypisano pompek")
-L.pack()
+b1 = Button(root, text="  +1 Pompka  ", command=lambda: LabelAdd(1), justify='center').grid(row=2, column=0)
+
+b2 = Button(root, text="  +3 Pompka  ", command=lambda: LabelAdd(3)).grid(row=3, column=0)
+
+b3 = Button(root, text="Reset", command=lambda: LabelSet(0)).grid(row=1, column=3)
+
+b4 = Button(root, text="Dodaj", command=lambda: FormSubmit()).grid(row=4, column=4)
+
+#Labels
+l1 = Label(root, textvariable=ldata).grid(row=1, column=2)
+
+l2 = Label(root, text="Pompki:").grid(row=1, column=1)
+
+l3 = Label(root, text="Dodaj Własne:").grid(row=2, column=3)
+
+#Entries
+entry1 = Entry(root, textvariable=pompki).grid(row=3, column=3) #Pompki
+entry2 = Entry(root, textvariable=powod).grid(row=4, column=3) #Powód
 
 root.mainloop()
