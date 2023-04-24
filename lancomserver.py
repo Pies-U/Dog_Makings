db603882296a25dce8e57b6b157d799341c2aeaa
Pies-U <ppiieessuu@gmail.com>
2023-04-24 06:54:25 +0200

    lancom v 0.1

--- lancomserver.py
+++ lancomserver.py
@@ -1,23 +1,47 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
 import socket
 import threading
+import time
 
+#Defines
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+#Control Variables 
+server_online = True
+close_connections = False
+
+
+#Console Config
+
+
+#Net Config
 PORT = 32222
-LOCAL_HOST = socket.gethostbyname(socket.gethostname())
+LOCAL_HOST = "192.168.1.10"
 ADDR = (LOCAL_HOST, PORT)
 
+#Console Code
+
+#Net Code
+
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
+    print(f"[NEW CONNECTION] {addr[0]} connected")
     while True:
         recived_data = conn.recv(1024).decode()
-        print(f'[RECIVED DATA] "{recived_data}" from {addr}')
+        print(f'[RECIVED DATA] "{recived_data}" from {addr[0]}')
+        if close_connections == True:
+            conn.close()
 
-def startup():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def listener():
     server.bind(ADDR)
     server.listen()
     print(f"[LISTENING] Server listening on {LOCAL_HOST} and port {PORT} ...")
     conn, addr = server.accept()
     threading.Thread(target=handle_client, args=(conn, addr)).start()
 
-startup()
 
+# Startup
+
+listener()
--- test.py
+++ test.py
@@ -1,17 +1,9 @@
-from rich.console import Console
-from rich.panel import Panel
+import time
+def changeline(msg):
+    print("Kij")
+    print(f'{msg}', end='\r')
+    time.sleep(1)
 
-def main():
-    # Set up the info panel
-    console = Console()
-    title = Panel("Info Panel", style="bold")
-    cpu_usage = "CPU usage: [bold green]50%[/]"
-    memory_usage = "Memory usage: [bold yellow]75%[/]"
-    disk_usage = "Disk usage: [bold blue]40%[/]"
-    content = "\n".join([cpu_usage, memory_usage, disk_usage])
-    panel = Panel(content, title=title)
 
-    # Print the info panel to the console
-    console.print(panel)
-
-main()
+time.sleep(1)
+changeline("hello")
