9fcc72dc18a4284f0c6a0f0a7689f34f0b44d8cc
Pies-U <91903894+Pies-U@users.noreply.github.com>
2024-12-23 19:57:42 +0100

    Update KeyGen.py
    Enable the choosing of custom keylengths

--- KeyGen.py
+++ KeyGen.py
@@ -1,11 +1,11 @@
 import rsa
 
-def genkyes():
-    publickey,privatekey = rsa.newkeys(1024)
+def genkyes(length):
+    publickey,privatekey = rsa.newkeys(int(length))
     with open("public.pem", "wb") as f:
         f.write(publickey.save_pkcs1("PEM"))
-    
+
     with open("private.pem", "wb") as f:
         f.write(privatekey.save_pkcs1("PEM"))
 
-genkyes()
+genkyes(input("KeyLength (1024 or 2048[Recommened])> "))
